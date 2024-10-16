e5546260346730d9797c3f58a32d2d8a30eb5341
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-10-16 20:04:32 +0530

    add basic program of java

---
+++ Basics/Basic.java
@@ -0,0 +1,17 @@
+class Basic {
+
+    public static void main(String[] args) {
+        // simple print
+        System.out.println("Hello World");
+
+        // variable's
+        // global variable
+
+        int temp = 10;
+        float percentage = 1.0f;
+
+        float total = (temp + percentage);
+
+        System.out.println("total test : " + total);    
+    }
+}
