999eea924f0fd1aac05617a578a9394a50f5c426
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-10-17 20:30:31 +0530

    constant value in java

--- Basics/Basic.java
+++ Basics/Basic.java
@@ -1,17 +1,20 @@
 class Basic {
+    // constant
+
+    static final double PI = 3.14;
 
     public static void main(String[] args) {
         // simple print
         System.out.println("Hello World");
 
-        // variable's
-        // global variable
+        // variable's #scope gloable
 
         int temp = 10;
         float percentage = 1.0f;
 
         float total = (temp + percentage);
 
-        System.out.println("total test : " + total);    
+        System.out.println("total test : " + total);
+        System.out.println("PI : " + PI);
     }
 }
