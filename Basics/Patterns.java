c423a2fd2515d54731251734b84870057d450c50
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-11-07 22:26:44 +0530

    add new pattern

--- Basics/Patterns.java
+++ Basics/Patterns.java
@@ -1,6 +1,5 @@
 public class Patterns {
     public static void main(String[] args) {
-
         System.out.println("Pattern 1");
         for (int i = 0; i < 5; i++) {
             System.out.println("*");
@@ -21,8 +20,8 @@ public class Patterns {
         for (int i = 0; i < 5; i++) {
             System.out.print("*");
         }
-        System.out.println();
 
+        System.out.println();
         System.out.println("Pattern 4");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -30,6 +29,37 @@ public class Patterns {
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        System.out.println("Pattern 5");
+        int n = 5;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                // Print '*' if it's the first or last row, or first or last column
+                if (i == 0 || i == 4 - 1 || j == 0 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        System.out.println("Pattern 6");
+        for (int i = 5; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Pattern 7");
+        
 
         // pattern 2
         // for (int i = 0; i < 5; i++) {
