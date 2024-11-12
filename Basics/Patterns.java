86b11f9e70ef589dcd94e397a14006d5dc2ef9c4
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-11-12 18:06:22 +0530

    static methods

--- Basics/Patterns.java
+++ Basics/Patterns.java
@@ -59,53 +59,52 @@ public class Patterns {
         System.out.println();
 
         System.out.println("Pattern 7");
-        
 
         // pattern 2
-        // for (int i = 0; i < 5; i++) {
-        // for (int j = 5; j > i; j--) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        // System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         // // pattern 3
-        // for (int i = 0; i < 5; i++) {
-        // for (int j = 0; j < i; j++) {
-        // System.out.print(" ");
-        // }
-        // for (int k = 5; k > i; k--) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k > i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // System.out.println();
+        System.out.println();
 
         // // pattern 4
-        // for (int i = 0; i < 5; i++) {
-        // for (int j = 5; j > i; j--) {
-        // System.out.print(" ");
-        // }
-        // for (int k = 0; k <= i; k++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
-        // System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         // // pattern 5
-        // for (int i = 0; i < 5; i++) {
-        // for (int j = 5; j > i; j--) {
-        // System.out.print(" ");
-        // }
-        // for (int k = 0; k <= i; k++) {
-        // System.out.print("* ");
-        // }
-        // System.out.println();
-        // }
-        // System.out.println();
+         for (int i = 5; i > 0; i--) {
+            for (int j = 0; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
 
         // // pattern 6
         // for (int i = 0; i < 5; i++) {
--- Basics/StaticVariable.java
+++ Basics/StaticVariable.java
@@ -3,11 +3,6 @@ class Mobiles {
     int price;
     String brand;
 
-    static { // static block is used to initialize the static variables and called only once
-        name = "Mobile";
-        System.out.println("Static block is called");
-    }
-
     // constructor to initialize the values
     public Mobiles() {
         price = 200;
@@ -17,23 +12,44 @@ class Mobiles {
     public void display() {
         System.out.println("Name: " + name + " Price: " + price + " Brand: " + brand);
     }
+
+    static { // static block is used to initialize the static variables and called only once
+        name = "Mobile";
+        System.out.println("Static block is called");
+    }
+
+    public static void testMethod() {
+        System.out.println("This is a static method");
+        System.out.println(name); // we can use static variable in static method but don't use non-static variable
+        // System.out.println(brand); // this will give error
+    }
+
+    // we can use non-static variable in static method but not vice versa
+    public static void testMethod2(Mobiles m) {
+        System.out.println("This is a non-static method");
+        System.out.println(name);
+        System.out.println(m.brand); // we can use non-static variable in static method with the help of passing
+                                     // object reference
+    }
 }
 
 public class StaticVariable {
     public static void main(String[] args) throws ClassNotFoundException {
 
-        // if we don't want to create object of the class and just call the class without creating object
+        // if we don't want to create object of the class and just call the class
+        // without creating object
         // then we can use Class and for that we have to throw ClassNotFoundException
         Class.forName("Mobiles");
 
         Mobiles m1 = new Mobiles();
-        Mobiles.name = "smartphone"; // static variable is shared among all objects so we can access it directly
-                                     // using class name
+        Mobiles.name = "smartphone"; // static variable is shared among all objects
+        // so we can access it directly
+        // using class name
         m1.price = 10000;
         m1.brand = "Samsung";
 
         Mobiles m2 = new Mobiles();
-        Mobiles.name = "smartphone";
+        Mobiles.name = "njbhiviv";
         m2.price = 100000;
         m2.brand = "Apple";
 
@@ -42,5 +58,10 @@ public class StaticVariable {
         m1.display();
         m2.display();
         m3.display();
+
+        // we can directly access static method using class name
+        Mobiles.testMethod();
+
+        Mobiles.testMethod2(m2);
     }
 }
