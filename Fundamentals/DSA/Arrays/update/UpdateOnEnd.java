72f9eb1bb90c8d3344d42f9a0692baa84b4c91c7
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-05 15:51:50 +0530

    update array last index

---
+++ Fundamentals/DSA/Arrays/update/UpdateOnEnd.java
@@ -0,0 +1,32 @@
+package Arrays.update;
+
+import java.util.Scanner;
+
+public class UpdateOnEnd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println("Your array is: ");
+        for (int i : arr) {
+            System.out.println(i + " ");
+        }
+
+        System.out.println("Enter the element you want to update: ");
+        int element = s.nextInt();
+
+        arr[arr.length - 1] = element;
+
+        System.out.println("Updated array is: ");
+        for(int i : arr) {
+            System.out.println(i + " ");
+        }
+    }
+}
