67446a80fcd4f6b9c1b21537bb63ab9b2123abc6
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-06 19:34:02 +0530

    add selection sort

---
+++ Fundamentals/DSA/Arrays/Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Arrays.Sorting;
+
+public class SelectionSort {
+
+    public static void SelectionSorting(int[] arr) {
+        // System.out.println("Lengt of array " + arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println((j < arr.length)+" for index"+j+" "+arr[j]);
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+            System.out.println("Pass "+ i + " : "+arr[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 64, 25, 12, 22, 11 };
+        SelectionSorting(arr);
+    }
+}
