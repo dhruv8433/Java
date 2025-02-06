0783cf7248fdd742ec88c303647c49e297b45406
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-06 19:33:53 +0530

    add insertion sort

---
+++ Fundamentals/DSA/Arrays/Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package Arrays.Sorting;
+
+public class InsertionSort {
+    public static void InsertionSorting(int arr[]) {
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+        InsertionSorting(arr);
+    }
+}
