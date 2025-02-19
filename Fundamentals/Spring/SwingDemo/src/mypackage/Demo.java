c108bc944e739dfb994d92edc32c76998fc01f63
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-19 13:57:34 +0530

    Demo

---
+++ Fundamentals/Spring/SwingDemo/src/mypackage/Demo.java
@@ -0,0 +1,85 @@
+package mypackage;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Demo extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txName;
+	private JTextField txEmail;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Demo frame = new Demo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Demo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		lblNewLabel.setBounds(29, 23, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(29, 97, 46, 14);
+		contentPane.add(lblEmail);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String textName = txName.getText();
+				String textEmail = txEmail.getText();
+				
+				System.out.println("Welcome back"+ textName + textEmail);
+			}
+		});
+		btnNewButton.setBounds(29, 170, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Reset");
+		btnNewButton_1.setBounds(206, 170, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		txName = new JTextField();
+		txName.setBounds(105, 20, 190, 20);
+		contentPane.add(txName);
+		txName.setColumns(10);
+		
+		txEmail = new JTextField();
+		txEmail.setBounds(105, 94, 190, 20);
+		contentPane.add(txEmail);
+		txEmail.setColumns(10);
+	}
+}
