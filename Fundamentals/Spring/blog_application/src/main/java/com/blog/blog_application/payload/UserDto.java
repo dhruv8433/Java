575e8aaa4e3adadc93cee8678c0d5e024daf492e
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-08 16:32:19 +0530

    user controller, user model and repo, dto and service implementation

---
+++ Fundamentals/Spring/blog_application/src/main/java/com/blog/blog_application/payload/UserDto.java
@@ -0,0 +1,22 @@
+package com.blog.blog_application.payload;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+    private int id;
+    @NotNull
+    private String name;
+    @Email
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String type;
+}
