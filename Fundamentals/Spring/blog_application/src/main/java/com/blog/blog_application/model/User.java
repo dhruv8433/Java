b3854d4047f76dceb001fecb4ce0cb15189adb4d
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-10 13:03:38 +0530

    post and user model and dtos

--- Fundamentals/Spring/blog_application/src/main/java/com/blog/blog_application/model/User.java
+++ Fundamentals/Spring/blog_application/src/main/java/com/blog/blog_application/model/User.java
@@ -1,10 +1,16 @@
 package com.blog.blog_application.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,4 +33,8 @@ public class User {
     private String email;
     private String password;
     private String type;
+
+      // one to many relationship with post
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<Post>();
 }
--- Fundamentals/Spring/blog_application/src/main/java/com/blog/blog_application/payload/UserDto.java
+++ Fundamentals/Spring/blog_application/src/main/java/com/blog/blog_application/payload/UserDto.java
@@ -1,7 +1,8 @@
 package com.blog.blog_application.payload;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,12 +12,19 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private int id;
-    @NotNull
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 3, message = "Name should have at least 3 characters")
     private String name;
-    @Email
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
-    @NotNull
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password should have at least 6 characters")
     private String password;
-    @NotNull
+
+    @NotBlank(message = "Type cannot be empty")
     private String type;
 }
